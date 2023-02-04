d0064bc43742c473e3d441619296087302465158
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-02-03 21:36:15 -0500

    redux and toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/profile:userId" element={<LoginPage />} />
+          <Route path="/profile:userId" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
