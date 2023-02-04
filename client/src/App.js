27b0bdaff58588e27130253b60e7142110752156
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-02-03 19:39:39 -0500

    folder architecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  const mode = use;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile:userId" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
